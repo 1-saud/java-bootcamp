520357cf6dffff6ed33fa40af9ca158f1fabdd59
1-saud <167019650+1-saud@users.noreply.github.com>
2025-10-13 23:24:53 +0300

    Create StringComparison.java

---
+++ StringComparison.java
@@ -0,0 +1,9 @@
+public class StringComparison {
+    public static void main(String[] args) {
+        String a = new String("Hello");
+        String b = new String("Hello");
+
+        System.out.println(a == b);      
+        System.out.println(a.equals(b));  
+    }
+}
